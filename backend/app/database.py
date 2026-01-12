7606c0092f5d75d4d7746473e5eda13f9b551e1b
Dadgbe <125733085+Dadgbe@users.noreply.github.com>
2026-01-12 20:58:17 +0300

    add models for category for products

--- backend/app/database.py
+++ backend/app/database.py
@@ -7,3 +7,19 @@ engine = create_engine(
     settings.database_url,
     connect_args={"check_same_thread": False}
 )
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
